572a85d38b1de7722d525c445e48c2088f002079
Arpan Majumdar <arpan.majumdar.dev@gmail.com>
2020-08-02 20:14:08 +0530

    Added a sample spark scala project

---
+++ big-data/spark/scala/spark-examples/build.sbt
@@ -0,0 +1,10 @@
+name := "spark-examples"
+
+version := "0.1"
+
+scalaVersion := "2.12.10"
+
+libraryDependencies ++= Seq(
+  "org.apache.spark" %% "spark-core" % "3.0.0",
+  "org.apache.spark" %% "spark-sql"  % "3.0.0"
+)
---
+++ big-data/spark/scala/spark-examples/src/main/scala/com/github/arpan/scala/sparkexamples/PeopleAges.scala
@@ -0,0 +1,26 @@
+package com.github.arpan.scala.sparkexamples
+
+import org.apache.spark.sql.SparkSession
+import org.apache.spark.sql.functions._
+
+object PeopleAges {
+
+  def main(args: Array[String]): Unit = {
+    val sparkSession = SparkSession
+      .builder
+      .appName("PeopleAges")
+      .master("local[*]")
+      .getOrCreate()
+
+    val dataDF = sparkSession.createDataFrame(Seq(
+      ("Brooke", 20),
+      ("Brooke", 25),
+      ("Denny", 31),
+      ("Jules", 30),
+      ("TD", 35))
+    ).toDF("name", "age")
+
+    val avgDf = dataDF.groupBy("name").agg(avg("age"))
+    avgDf.show()
+  }
+}
