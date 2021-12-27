3aa62095cf0b5668a40b1214a59db0b5611d9e67
Arpan Majumdar <arpan.majumdar.dev@gmail.com>
2021-12-27 22:19:30 +0530

    Earthquakes tooltip completed

--- frontend/javascript/d3-js-tutorial/html-overview/earthquakes.js
+++ frontend/javascript/d3-js-tutorial/html-overview/earthquakes.js
@@ -6,6 +6,12 @@ const svg = canvas.append("svg").attr("width", width).attr("height", height);
 const earthquakeApiUrl =
   "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/significant_month.geojson";
 
+const div = d3
+  .select("body")
+  .append("div")
+  .attr("class", "tooltip")
+  .style("opacity", 0);
+
 // Parse JSON
 d3.json(earthquakeApiUrl).then((data) => {
   const circle = svg.selectAll("circle").data(data["features"]);
@@ -27,10 +33,30 @@ d3.json(earthquakeApiUrl).then((data) => {
     })
     .attr("stroke", "black")
     .on("mouseover", function (event, d) {
-      console.log(d["properties"]["mag"]);
+      // console.log(d["properties"]["mag"]);
+      // console.log(event);
       d3.select(this).transition().duration(100).style("opacity", 0.3);
+
+      div.transition().duration(200).style("opacity", 0.8);
+      div
+        .html(
+          `<p>Mag: ${d["properties"]["mag"]}<\p>
+        <p>Date: ${longTimestampToDate(d["properties"]["time"])}<\p>
+        <p>Place: ${extractLocation(d["properties"]["place"])}`
+        )
+        .style("left", `${event.pageX}px`)
+        .style("top", `${event.pageY - 40}px`);
     })
     .on("mouseout", function (event, d) {
       d3.select(this).transition().duration(100).style("opacity", 1);
+      div.transition().duration(400).style("opacity", 0);
     });
 });
+
+function longTimestampToDate(timestamp) {
+  return new Date(timestamp).toLocaleDateString("en-US");
+}
+
+function extractLocation(place) {
+  return place.split(",")[1].trim();
+}
